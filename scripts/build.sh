d217c5cddfc10bd3e3b3044a0d6a44700a2d5383
Thomas Pal <thomas.pal@tuhh.de>
2024-06-30 22:21:38 +0200

    Update build script to use the linker script for RPI4 config
    RPI4 is the only config for now. Need to generalize the mechanism.

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
+SCRIPT_DIR=$(dirname $(readlink -f $0))
 EXTRA_ARGS=""
 
 if [ "$1" == "release" ]; then
     EXTRA_ARGS="--release"
 fi
 
-cargo build --target=aarch64-unknown-none-softfloat $EXTRA_ARGS
+
+# TODO: Make it configurable depending on the selected board/architecture
+LINKER_LIBRARY_PATH=$SCRIPT_DIR/../src/board/raspi4/
+LINKER_SCRIPT="linking.ld"
+
+RUSTFLAGS="-C link-arg=--library-path=$LINKER_LIBRARY_PATH -C link-arg=--script=$LINKER_SCRIPT -C target-cpu=cortex-a72"
+
+cargo rustc --target=aarch64-unknown-none-softfloat $EXTRA_ARGS -- $RUSTFLAGS
